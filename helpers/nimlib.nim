efd747e13e4dcb59f8ad48c592828f1a12a54f4c
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-04-25 23:13:42 +0300

    Remove redundant export annotations in nimlib

--- helpers/nimlib.nim
+++ helpers/nimlib.nim
@@ -10,34 +10,34 @@ macro importAll(lib: static[string], body: untyped): untyped =
       result.add(node)
 
 importAll("/path/to/nimlib.so"):
-  proc allocbuf*(size: int): pointer {.exportc, dynlib.}
-  proc cf*(path: cstring) {.exportc, dynlib.}
-  proc cls*() {.exportc, dynlib.}
-  proc def*(name: cstring; value: cstring) {.exportc, dynlib.}
-  proc exec*(command: cstring): tuple[stdo: cstring, exit_code: int] {.exportc, dynlib.}
-  proc filesize*(path: cstring): int {.exportc, dynlib.}
-  proc fileread*(path: cstring): cstring {.exportc, dynlib.}
-  proc filewrite*(path: cstring; content: cstring) {.exportc, dynlib.}
-  proc freebuf*(pointer1: pointer) {.exportc, dynlib.}
-  proc getcf*(): cstring {.exportc, dynlib.}
-  proc getdef*(name: cstring): cstring {.exportc, dynlib.}
-  proc getprocid*(): int {.exportc, dynlib.}
-  proc halt*(exit_code: int) {.exportc, dynlib.}
-  proc isdef*(name: cstring): int {.exportc, dynlib.}
-  proc isfile*(path: cstring): int {.exportc, dynlib.}
-  proc isfolder*(path: cstring): int {.exportc, dynlib.}
-  proc isroot*(): int {.exportc, dynlib.}
-  proc lf*(): tuple[paths: ptr UncheckedArray[cstring], types_of: ptr UncheckedArray[cstring], len: cint] {.exportc, dynlib.}
-  proc mcopy*(source: cstring; destination: pointer; size: int) {.exportc, dynlib.}
-  proc mkfile*(path: cstring) {.exportc, dynlib.}
-  proc mkfolder*(path: cstring) {.exportc, dynlib.}
-  proc reallocbuf*(pointer1: pointer; new_size: int): pointer {.exportc, dynlib.}
-  proc rm*(path: cstring) {.exportc, dynlib.}
-  proc rmfolder*(path: cstring) {.exportc, dynlib.}
-  proc mv*(old_path: cstring; new_path: cstring) {.exportc, dynlib.}
-  proc mvfolder*(old_path: cstring; new_path: cstring) {.exportc, dynlib.}
-  proc stdi*(): cstring {.exportc, dynlib.}
-  proc stdo*(string1: cstring) {.exportc, dynlib.}
-  proc strcomp*(string1: cstring; string2: cstring): int {.exportc, dynlib.}
-  proc undef*(name: cstring) {.exportc, dynlib.}
-  proc wait*(milliseconds: int) {.exportc, dynlib.}
+  proc allocbuf*(size: int): pointer
+  proc cf*(path: cstring)
+  proc cls*()
+  proc def*(name: cstring; value: cstring)
+  proc exec*(command: cstring): tuple[stdo: cstring, exit_code: int]
+  proc filesize*(path: cstring): int
+  proc fileread*(path: cstring): cstring
+  proc filewrite*(path: cstring; content: cstring)
+  proc freebuf*(pointer1: pointer)
+  proc getcf*(): cstring
+  proc getdef*(name: cstring): cstring
+  proc getprocid*(): int
+  proc halt*(exit_code: int)
+  proc isdef*(name: cstring): int
+  proc isfile*(path: cstring): int
+  proc isfolder*(path: cstring): int
+  proc isroot*(): int
+  proc lf*(): tuple[paths: ptr UncheckedArray[cstring], types_of: ptr UncheckedArray[cstring], len: cint]
+  proc mcopy*(source: cstring; destination: pointer; size: int)
+  proc mkfile*(path: cstring)
+  proc mkfolder*(path: cstring)
+  proc reallocbuf*(pointer1: pointer; new_size: int): pointer
+  proc rm*(path: cstring)
+  proc rmfolder*(path: cstring)
+  proc mv*(old_path: cstring; new_path: cstring)
+  proc mvfolder*(old_path: cstring; new_path: cstring)
+  proc stdi*(): cstring
+  proc stdo*(string1: cstring)
+  proc strcomp*(string1: cstring; string2: cstring): int
+  proc undef*(name: cstring)
+  proc wait*(milliseconds: int)
