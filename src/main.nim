36fd600728086ef13c80e1dd3fffefe6544f9569
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-04-25 21:16:06 +0300

    Add code

---
+++ src/main.nim
@@ -0,0 +1,110 @@
+import osproc
+import std/os
+
+proc allocbuf*(size: int): pointer {.exportc, dynlib.} =
+  return alloc(size)
+
+proc cf*(path: cstring) {.exportc, dynlib.} =
+  setCurrentDir($path)
+
+proc cls*() {.exportc, dynlib.} =
+  stdout.write("\x1b[2J\x1b[H")
+  stdout.flushFile()
+
+proc def*(name: cstring; value: cstring) {.exportc, dynlib.} =
+  putEnv($name, $value)
+
+proc exec*(command: cstring): tuple[stdo: cstring, exit_code: int] {.exportc, dynlib.} =
+  let (stdo, exit_code) = execCmdEx($command)
+  return (cstring(stdo), int(exit_code))
+
+proc filesize*(path: cstring): int {.exportc, dynlib.} =
+  return int(getFileSize($path))
+
+proc fileread*(path: cstring): cstring {.exportc, dynlib.} =
+  return cstring(readFile($path))
+
+proc filewrite*(path: cstring; content: cstring) {.exportc, dynlib.} =
+  writeFile($path, $content)
+
+proc freebuf*(pointer1: pointer) {.exportc, dynlib.} =
+  dealloc(pointer1)
+
+proc getcf*(): cstring {.exportc, dynlib.} =
+  return cstring(getCurrentDir())
+
+proc getdef*(name: cstring): cstring {.exportc, dynlib.} =
+  if existsEnv($name):
+    return cstring($getEnv($name))
+  else:
+    return ""
+
+proc getprocid*(): int {.exportc, dynlib.} =
+  return int(getCurrentProcessId())
+
+proc halt*(exit_code: int) {.exportc, dynlib.} =
+  quit(exit_code)
+
+proc isdef*(name: cstring): int {.exportc, dynlib.} =
+  return int(existsEnv($name) == true)
+
+proc isfile*(path: cstring): int {.exportc, dynlib.} =
+  return int(fileExists($path) == true)
+
+proc isfolder*(path: cstring): int {.exportc, dynlib.} =
+  return int(dirExists($path) == true)
+
+proc isroot*(): int {.exportc, dynlib.} =
+  return int(isAdmin() == true)
+
+proc lf*(): tuple[paths: ptr UncheckedArray[cstring], types_of: ptr UncheckedArray[cstring], len: cint] {.exportc, dynlib.} =
+  var p: seq[cstring]
+  var t: seq[cstring]
+  for kind, name in walkDir(getCurrentDir()):
+    if kind == pcFile:
+      p.add(cstring(name)); t.add(cstring("file"))
+    elif kind == pcDir:
+      p.add(cstring(name)); t.add(cstring("folder"))
+  result = (cast[ptr UncheckedArray[cstring]](addr p[0]), cast[ptr UncheckedArray[cstring]](addr t[0]), p.len.cint)
+
+proc mcopy*(source: cstring; destination: pointer; size: int) {.exportc, dynlib.} =
+  copyMem(destination, source, size)
+
+proc mkfile*(path: cstring) {.exportc, dynlib.} =
+  writeFile($path, "")
+
+proc mkfolder*(path: cstring) {.exportc, dynlib.} =
+  createDir($path)
+
+proc reallocbuf*(pointer1: pointer; new_size: int): pointer {.exportc, dynlib.} =
+  return realloc(pointer1, new_size)
+
+proc rm*(path: cstring) {.exportc, dynlib.} =
+  removeFile($path)
+
+proc rmfolder*(path: cstring) {.exportc, dynlib.} =
+  removeDir($path)
+
+proc mv*(old_path: cstring; new_path: cstring) {.exportc, dynlib.} =
+  moveFile($old_path, $new_path)
+
+proc mvfolder*(old_path: cstring; new_path: cstring) {.exportc, dynlib.} =
+  moveDir($old_path, $new_path)
+
+proc stdi*(): cstring {.exportc, dynlib.} =
+  try:
+    return cstring(readLine(stdin))
+  except EOFError:
+    return ""
+
+proc stdo*(string1: cstring) {.exportc, dynlib.} =
+  write(stdout, string1)
+
+proc strcomp*(string1: cstring; string2: cstring): int {.exportc, dynlib.} =
+  return int(cmp(string1, string2) != 0)
+
+proc undef*(name: cstring) {.exportc, dynlib.} =
+  delEnv($name)
+
+proc wait*(milliseconds: int) {.exportc, dynlib.} =
+  sleep(milliseconds)
