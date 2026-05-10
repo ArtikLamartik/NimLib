7de27a6bce35f66bcf7e1193295adb27243fa348
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-05-10 21:45:25 +0300

    Update nimlib.c

--- importers/nimlib.c
+++ importers/nimlib.c
@@ -1,39 +1,53 @@
 typedef struct {
-    const char* stdo;
-    int exit_code;
+    const char* output;
+    int         exit_code;
 } Exec;
 
 typedef struct {
-    const char** args;
-    int length;
-} Getargs;
+    const char* name;
+    const char* creator;
+    long        last_edit;
+    long        file_size;
+} Fileinfo;
 
 typedef struct {
     const char* name;
-    const char* process_id;
-    const char* parent_process_id;
-    const char* user_id;
-    const char* start_time;
-    const char* command;
-} Infoproc;
+    const char* creator;
+    long        last_edit;
+    long        folder_size;
+} Folderinfo;
+
+typedef struct {
+    const char** args;
+    int          length;
+} Getargs;
 
 typedef struct {
     const char** name;
     const char** value;
-    int length;
+    int          length;
 } Ladefs;
 
 typedef struct {
     const char** name;
     const char** process_id;
-    int length;
+    int          length;
 } Laprocs;
 
 typedef struct {
     const char** paths;
     const char** types_of;
-    int length;
+    int          length;
 } Lf;
+
+typedef struct {
+    const char* name;
+    const char* process_id;
+    const char* parent_process_id;
+    const char* user_name;
+    const char* start_time;
+    const char* command;
+} Procinfo;
 
 extern void*       allocbuf   (int size);
 extern void        cf         (const char* path);
@@ -42,9 +56,10 @@ extern void        cursor     (int visible);
 extern void        cursorto   (int x, int y);
 extern void        def        (const char* name, const char* value);
 extern Exec        exec       (const char* command);
+extern Fileinfo    fileinfo   (const char* path);
 extern const char* fileread   (const char* path);
-extern int         filesize   (const char* path);
 extern void        filewrite  (const char* path, const char* content);
+extern Folderinfo  folderinfo (const char* path);
 extern void        freebuf    (void* pointer1);
 extern Getargs     getargs    (void);
 extern const char* getcf      (void);
@@ -53,7 +68,6 @@ extern int         getcpid    (void);
 extern const char* getdef     (const char* name);
 extern const char* getprogloc (void);
 extern void        halt       (int exit_code);
-extern Infoproc    infoproc   (int process_id);
 extern int         isdef      (const char* name);
 extern int         isfile     (const char* path);
 extern int         isfolder   (const char* path);
@@ -68,6 +82,7 @@ extern void        mkfolder   (const char* path);
 extern int         msgbox     (const char* title, int count, ...);
 extern void        mvfile     (const char* old_path, const char* new_path);
 extern void        mvfolder   (const char* old_path, const char* new_path);
+extern Procinfo    procinfo   (int process_id);
 extern int         randint    (int minimum, int maximum);
 extern void*       reallocbuf (void* pointer1, int new_size);
 extern void        resetbgfg  (void);
@@ -82,8 +97,9 @@ extern const char* stdi       (int visible);
 extern void        stdo       (const char* string1);
 extern int         strcomp    (const char* string1, const char* string2);
 extern const char* strformat  (int count, ...);
-extern int         tocintcstr (const char* value);
-extern const char* tocstrcint (int value);
+extern int         timern     (void);
+extern int         tointstr   (const char* value);
+extern const char* tostrint   (int value);
 extern void        undef      (const char* name);
 extern void        wait       (int milliseconds);
 extern const char* where      (const char* command);
