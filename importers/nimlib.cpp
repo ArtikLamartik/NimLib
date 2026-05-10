b352c0297835e8df1ad40788d37b694ef2d51f2f
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-05-10 21:45:48 +0300

    Update nimlib.cpp

--- importers/nimlib.cpp
+++ importers/nimlib.cpp
@@ -1,38 +1,52 @@
 struct Exec {
-    const char* stdo;
+    const char* output;
     int exit_code;
+};
+
+struct Fileinfo {
+    const char* name;
+    const char* creator;
+    long        last_edit;
+    long        file_size;
+};
+
+struct Folderinfo {
+    const char* name;
+    const char* creator;
+    long        last_edit;
+    long        folder_size;
 };
 
 struct Getargs {
     const char** args;
-    int length;
-};
-
-struct Infoproc {
-    const char* name;
-    const char* process_id;
-    const char* parent_process_id;
-    const char* user_id;
-    const char* start_time;
-    const char* command;
+    int          length;
 };
 
 struct Ladefs {
     const char** name;
     const char** value;
-    int length;
+    int          length;
 };
 
 struct Laprocs {
     const char** name;
     const char** process_id;
-    int length;
+    int          length;
 };
 
 struct Lf {
     const char** paths;
     const char** types_of;
-    int length;
+    int          length;
+};
+
+struct Procinfo {
+    const char* name;
+    const char* process_id;
+    const char* parent_process_id;
+    const char* user_name;
+    const char* start_time;
+    const char* command;
 };
 
 extern "C" {
@@ -43,9 +57,10 @@ extern "C" {
     void        cursorto   (int x, int y);
     void        def        (const char* name, const char* value);
     Exec        exec       (const char* command);
+    Fileinfo    fileinfo   (const char* path);
     const char* fileread   (const char* path);
-    int         filesize   (const char* path);
     void        filewrite  (const char* path, const char* content);
+    Folderinfo  folderinfo (const char* path);
     void        freebuf    (void* pointer1);
     Getargs     getargs    (void);
     const char* getcf      (void);
@@ -54,7 +69,6 @@ extern "C" {
     const char* getdef     (const char* name);
     const char* getprogloc (void);
     void        halt       (int exit_code);
-    Infoproc    infoproc   (int process_id);
     int         isdef      (const char* name);
     int         isfile     (const char* path);
     int         isfolder   (const char* path);
@@ -69,6 +83,7 @@ extern "C" {
     int         msgbox     (const char* title, int count, ...);
     void        mvfile     (const char* old_path, const char* new_path);
     void        mvfolder   (const char* old_path, const char* new_path);
+    Procinfo    procinfo   (int process_id);
     int         randint    (int minimum, int maximum);
     void*       reallocbuf (void* pointer1, int new_size);
     void        resetbgfg  (void);
@@ -83,8 +98,9 @@ extern "C" {
     void        stdo       (const char* string1);
     int         strcomp    (const char* string1, const char* string2);
     const char* strformat  (int count, ...);
-    int         tocintcstr (const char* value);
-    const char* tocstrcint (int value);
+    int         timern     (void);
+    int         tointstr   (const char* value);
+    const char* tostrint   (int value);
     void        undef      (const char* name);
     void        wait       (int milliseconds);
     const char* where      (const char* command);
