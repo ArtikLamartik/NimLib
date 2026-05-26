f56c96b57a98a827f44cd532f306674e421b0dd3
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-05-26 07:16:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-nim c --app:lib --out:build/nimlib.so src/main.nim
+
+build() {
+    nim c --app:lib --out:build/nimlib.so src/main.nim
+}
+
+install() {
+    sudo cp build/nimlib.so /usr/lib32/
+    sudo ldconfig
+    echo "Installed."
+}
+
+for arg in "$@"; do
+    case "$arg" in
+        --build|-b) build ;;
+        --install|-i) install ;;
+        *) echo "Unknown argument: $arg" ;;
+    esac
+done
