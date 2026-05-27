957e6c6326395c515fe824d8251e84fe3776783e
Artik Lamartik <137715094+ArtikLamartik@users.noreply.github.com>
2026-05-27 20:50:47 +0300

    Update nimlib.nim

--- importers/nimlib.nim
+++ importers/nimlib.nim
@@ -34,7 +34,7 @@ importAll("/usr/lib32/nimlib.so"):
   proc getprogloc* (): cstring
   proc getermsize* (): tuple[columns: int, rows: int]
   proc halt*       (atexit: int, exit_code: int)
-  proc has*        (string1: cstring, pattern: cstring): int
+  proc has*        (text: cstring, pattern: cstring): int
   proc isdef*      (name: cstring): int
   proc isfile*     (path: cstring): int
   proc isfolder*   (path: cstring): int
@@ -45,7 +45,7 @@ importAll("/usr/lib32/nimlib.so"):
   proc ladefs*     (): tuple[name: ptr UncheckedArray[cstring], value: ptr UncheckedArray[cstring], length: int]
   proc laprocs*    (): tuple[name: ptr UncheckedArray[cstring], process_id: ptr UncheckedArray[int], length: int]
   proc lf*         (): tuple[paths: ptr UncheckedArray[cstring], type_of: ptr UncheckedArray[cstring], length: int]
-  proc lowstr*     (string1: cstring): cstring
+  proc lowstr*     (text: cstring): cstring
   proc mcopy*      (source: cstring; destination: pointer; size: int)
   proc mkfile*     (path: cstring)
   proc mkfolder*   (path: cstring)
@@ -55,15 +55,15 @@ importAll("/usr/lib32/nimlib.so"):
   proc procinfo*   (process_id: int): tuple[name: cstring, process_id: int, parent_process_id: int, user_name: cstring, start_time: int, command: cstring]
   proc randint*    (minimum: int, maximum: int): int
   proc reallocbuf* (pointer1: pointer; new_size: int): pointer
-  proc replacestr* (string1: cstring, pattern: cstring, replacement: cstring): cstring
-  proc repstr*     (string1: cstring, count: int): cstring
+  proc replacestr* (text: cstring, pattern: cstring, replacement: cstring): cstring
+  proc repstr*     (text: cstring, count: int): cstring
   proc resetbgfg*  ()
-  proc revstr*     (string1: cstring): cstring
+  proc revstr*     (text: cstring): cstring
   proc rmfile*     (path: cstring)
   proc rmfolder*   (path: cstring)
   proc schedule*   (milliseconds: int, function: proc() {.noconv.})
   proc scope*      (atexit: int, function: proc() {.noconv.})
-  proc search*     (string1: cstring, pattern: cstring): tuple[matches: ptr UncheckedArray[cstring], length: int]
+  proc search*     (text: cstring, pattern: cstring): tuple[matches: ptr UncheckedArray[cstring], length: int]
   proc setbg*      (r: int, g: int, b: int)
   proc setfg*      (r: int, g: int, b: int)
   proc sig*        (signal: int, function: proc() {.noconv.})
@@ -76,13 +76,14 @@ importAll("/usr/lib32/nimlib.so"):
   proc socksend*   (data: cstring)
   proc spawnproc*  (command: cstring): int
   proc spawnthr*   (function: proc() {.noconv.})
-  proc splitstr*   (string1: cstring, pattern: cstring): tuple[parts: ptr UncheckedArray[cstring], length: int]
+  proc splitstr*   (text: cstring, pattern: cstring): tuple[parts: ptr UncheckedArray[cstring], length: int]
   proc stdi*       (visible: int): cstring
-  proc stdo*       (string1: cstring)
-  proc strcomp*    (string1: cstring; string2: cstring): int
+  proc stdo*       (text: cstring)
+  proc strcomp*    (text1: cstring; text2: cstring): int
   proc strformat*  (count: int): cstring {.varargs.}
-  proc strsize*    (string1: cstring): int
-  proc substr*     (string1: cstring, start_index: int, stop_index: int): cstring
+  proc strinsert*  (text1: cstring, text2: cstring, index: int): cstring
+  proc strsize*    (text: cstring): int
+  proc substr*     (text: cstring, start_index: int, stop_index: int): cstring
   proc syncthr*    (function: proc() {.noconv.})
   proc timern*     (): float
   proc timerstart* (name: cstring)
@@ -97,10 +98,10 @@ importAll("/usr/lib32/nimlib.so"):
   proc tostrflt*   (value: float): cstring
   proc tostrhex*   (value: cstring): cstring
   proc tostrint*   (value: int): cstring
-  proc trimstr*    (sides: int, string1: cstring, pattern: cstring): cstring
+  proc trimstr*    (text: cstring, sides: int, pattern: cstring): cstring
   proc undef*      (name: cstring)
   proc until*      (timestamp: float)
-  proc upstr*      (string1: cstring): cstring
+  proc upstr*      (text: cstring): cstring
   proc version*    (): cstring
   proc vidbuf*     (mode: int)
   proc wait*       (milliseconds: int)
